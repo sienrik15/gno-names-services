6702f1eaff6e7a66a143d41e676b9351c1bcd3d1
enriquesam <enrique_sam11@hotmail.com>
2022-12-01 18:57:05 -0500

    add favicon ico

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -29,6 +29,8 @@ class MyDocument extends Document {
                href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
                rel="stylesheet"
             />
+            <title>Gno Domains</title>
+            <link rel="icon" href="/favicon-white.ico"/>
             <Head>{CssBaseline.flush()}</Head>
 
             <body>
